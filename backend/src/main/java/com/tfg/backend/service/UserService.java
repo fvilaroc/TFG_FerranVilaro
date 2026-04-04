e598d693af84e113ebe15e22190c14308d5242e7
fvilaroc <165994229+fvilaroc@users.noreply.github.com>
2026-04-04 16:49:19 +0200

    UserService

--- backend/src/main/java/com/tfg/backend/service/UserService.java
+++ backend/src/main/java/com/tfg/backend/service/UserService.java
@@ -1,4 +1,32 @@
 package com.tfg.backend.service;
 
+import com.tfg.backend.domain.User;
+import com.tfg.backend.persistance.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getCurrentUser(String username) {
+        return userRepository.findByUsername(username).
+                orElseThrow(() -> new RuntimeException("User not found with username: " + username));
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).
+                orElseThrow(() -> new RuntimeException("User not found with id: " + id));
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
