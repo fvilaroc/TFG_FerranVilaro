1b9d8d14984f44234da50776c2a2b8f78d40d387
fvilaroc <165994229+fvilaroc@users.noreply.github.com>
2026-04-05 15:49:30 +0200

    Update RankingController.java

--- backend/src/main/java/com/tfg/backend/controller/RankingController.java
+++ backend/src/main/java/com/tfg/backend/controller/RankingController.java
@@ -1,4 +1,29 @@
 package com.tfg.backend.controller;
 
+import com.tfg.backend.service.RankingService;
+import com.tfg.backend.service.dto.DanceRankingDTO;
+import com.tfg.backend.service.dto.GlobalRankingDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/ranking")
 public class RankingController {
+
+    private final RankingService rankingService;
+
+    public RankingController(RankingService rankingService) {
+        this.rankingService = rankingService;
+    }
+
+    @GetMapping("/global")
+    public List<GlobalRankingDTO> getGlobalRanking() {
+        return rankingService.getGlobalRanking();
+    }
+
+    @GetMapping("/dance/{danceId}")
+    public List<DanceRankingDTO> getDanceRanking(@PathVariable Long danceId) {
+        return rankingService.getDanceRanking(danceId);
+    }
 }
