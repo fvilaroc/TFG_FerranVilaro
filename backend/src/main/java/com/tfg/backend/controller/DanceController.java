f74075bec03fc98535a6febe1829f2df0c00a421
fvilaroc <165994229+fvilaroc@users.noreply.github.com>
2026-04-05 14:03:52 +0200

    UserController

--- backend/src/main/java/com/tfg/backend/controller/DanceController.java
+++ backend/src/main/java/com/tfg/backend/controller/DanceController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/dances")
+@RequestMapping("/api/dances")
 public class DanceController {
 
     private final DanceService danceService;
--- backend/src/main/java/com/tfg/backend/controller/UserController.java
+++ backend/src/main/java/com/tfg/backend/controller/UserController.java
@@ -1,4 +1,36 @@
 package com.tfg.backend.controller;
 
+import com.tfg.backend.service.UserService;
+import com.tfg.backend.service.dto.UserDTO;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/currentUser")
+    public UserDTO getCurrentUser(Authentication authentication) {
+        return userService.getCurrentUser(authentication.getName());
+    }
+
+    @GetMapping("/all")
+    public List<UserDTO> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public UserDTO getUserById(Long id) {
+        return userService.getUserById(id);
+    }
 }
