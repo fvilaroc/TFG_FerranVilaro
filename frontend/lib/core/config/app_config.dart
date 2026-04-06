6988f7f86e07a3742e051896cce07b3f82cde732
fvilaroc <165994229+fvilaroc@users.noreply.github.com>
2026-04-06 21:03:59 +0200

    frontend

---
+++ frontend/lib/core/config/app_config.dart
@@ -0,0 +1,12 @@
+class AppConfig {
+  // CAMBIA ESTA URL SEGUN DONDE EJECUTES LA APP
+
+  // Para Chrome/Web normalmente:
+  static const String baseUrl = 'http://localhost:8080';
+
+  // Para emulador Android sería:
+  // static const String baseUrl = 'http://10.0.2.2:8080';
+
+  // Para movil fisico sería algo como:
+  // static const String baseUrl = 'http://192.168.1.100:8080';
+}
