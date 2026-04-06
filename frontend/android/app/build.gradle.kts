e4913a18b8a0b7cf937b1e4c78aa6c1818ebff6e
fvilaroc <165994229+fvilaroc@users.noreply.github.com>
2026-04-06 21:05:05 +0200

    Revert "frontend"
    This reverts commit 6988f7f86e07a3742e051896cce07b3f82cde732.

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.frontend"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "30.0.14904198"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
